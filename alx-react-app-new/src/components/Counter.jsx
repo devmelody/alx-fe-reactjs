9423fdb92585b854bd8c043e5140d000d97a2549
Mel <melodynk01@gmail.com>
2025-07-17 16:49:46 +0100

    Added counter component and rectified errors

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -8,8 +8,8 @@ export default function Counter() {
         <div>
             <p>{count}</p>
             <div style={{border: '1px solid white'}}>
-            <button onClick={() => setCount(count + 1)}>+</button>
-            <button onClick={() => setCount(count - 1)}>-</button>
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <button onClick={() => setCount(count - 1)}>Decrement</button>
             <button onClick={() => setCount(0)}>Reset</button>
             </div>
         </div>
