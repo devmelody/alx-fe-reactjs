feac1891262cac519df4ce26fd40e59bf4016e6f
Mel <melodynk01@gmail.com>
2025-07-17 16:31:00 +0100

    Added duplicate react app folder

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p style={{fontStyle: 'italic'}}>© 2023 City Lovers</p>
+    </footer>
+  );
+}
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,7 @@
+export default function MainContent() {
+  return (
+    <main>
+      <p style={{color: 'orange'}}>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
