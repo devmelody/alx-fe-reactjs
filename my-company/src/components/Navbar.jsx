d96392a0ee629c3538d824c29ea16d666b2ce7f8
Mel <melodynk01@gmail.com>
2025-07-20 22:38:03 +0100

    Added styling to the Navbar

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,14 +1,29 @@
 import { Link } from "react-router-dom";
 
 function Navbar() {
-    return (
-        <nav style={{ padding: '1rem', borderBottom: '1px solid #ccc' }}>
-            <Link to="/" style={{margin: '1rem'}}>Home</Link>
-            <Link to="/about" style={{margin: '1rem'}}>About</Link>
-            <Link to="/contact" style={{margin: '1rem'}}>Contact</Link>
-            <Link to="/services" style={{margin: '1rem'}}>Services</Link>
-        </nav>
-    )
-
+  return (
+    <nav
+      style={{
+        backgroundColor: "#333",
+        display: "flex",
+        justifyContent: "center",
+        padding: "1rem",
+        gap: "1.5rem",
+      }}
+    >
+      <Link to="/" style={{ margin: "1rem" }}>
+        Home
+      </Link>
+      <Link to="/about" style={{ margin: "1rem" }}>
+        About
+      </Link>
+      <Link to="/contact" style={{ margin: "1rem" }}>
+        Contact
+      </Link>
+      <Link to="/services" style={{ margin: "1rem" }}>
+        Services
+      </Link>
+    </nav>
+  );
 }
 export default Navbar;
