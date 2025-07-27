3ab6df31fbb1d23a3d0ff6a277dfa5bb9646b1b2
Mel <melodynk01@gmail.com>
2025-07-27 21:43:37 +0100

    integrated routing for components

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,15 +1,25 @@
 import AddRecipeForm from './components/AddRecipeForm'
 import RecipeList from './components/RecipeList'
 import './App.css'
+import { BrowserRouter, Router, Routes, Route } from 'react-router-dom'
+import RecipeDetails from './components/RecipeDetails'
 
 
 function App() {
 
   return (
+   
+    <Routes>
+      <Route path='/' element={
+
     <>
      <AddRecipeForm />
      <RecipeList />
     </>
+      } />
+      <Route path='/recipe/:id' element={<RecipeDetails />} />
+    </Routes>
+
   )
 }
 
---
+++ task-tracker/src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-r from-blue-500 to-purple-600 text-white flex items-center justify-center text-3xl">
+      Tailwind v4 + Vite + React 🎉
+    </div>
+  );
+}
+
+
+export default App
