c3317b98a43ab9e1c2f091e7c852395601ed725d
Mel <melodynk01@gmail.com>
2025-08-10 16:02:56 +0100

    Styled container properly

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto, my-20 rounded-lg shadow-lg hover:shadow-xl">
+    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto, my-20 rounded-lg shadow-lg hover:shadow-xl sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm">
       <img src="https://placehold.co/400" alt="User"
       className="rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36 hover:scale-110 transition-transform duration-300 ease-in-out" />
       <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl hover:text-blue-500">John Doe</h1>
