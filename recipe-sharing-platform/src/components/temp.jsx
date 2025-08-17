810717f7181610be0a64aa49e52005a19e7c2efe
Mel <melodynk01@gmail.com>
2025-08-17 17:22:34 +0100

    Temp rename HomePage.jsx to temp.jsx

---
+++ recipe-sharing-platform/src/components/temp.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import recipeData from "../data.json";
+import { Link } from "react-router-dom";
+
+function HomePage() {
+  const [recipes, setRecipe] = useState([]);
+
+  useEffect(() => {
+    setRecipe(recipeData);
+  }, []);
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 place-items-center p-4 ">
+
+        <h1 className="text-xl font-bold mb-4 text-center col-span-full">Recipe finder</h1>
+        {recipes.map((recipe) => (
+          <Link to={`/recipe/${recipe.id}`}>
+
+            <div className="flex flex-col items-center" key={recipe.id}>
+                    <img className="w-24 h-24 shadow hover:shadow-lg transition-shadow rounded-xl" src={recipe.image} />
+                
+                    <p className="text-xs font-serif mt-2">{recipe.title}</p>
+                    <p className="text-sm font-semibold mt-1 mb-4 text-center">{recipe.summary}</p>
+                    
+            </div>
+          </Link>
+        ))}
+    </div>
+  )
+}
+export default HomePage;
